ebf161bcf7fd7eb254889f0090255d5ab679d03e
Eugene <ebabin000@gmail.com>
2024-03-28 15:28:57 +0200

    add mobile list open

--- src/components/layouts/header/navMenuMobile/index.tsx
+++ src/components/layouts/header/navMenuMobile/index.tsx
@@ -9,6 +9,47 @@ const navList = [
   {name: 'Resources', link: ''}
 ]
 
+const toolsList = [
+  { name: "Currency Charts", description: "Trends for any currency", link: "CurrencyCharts" },
+  { name: "IBAN Calculator", description: "Search and validate IBANs", link: "IBANCalculator" },
+  { name: "Rate Alerts", description: "Set your target rate and get alerted", link: "RateAlerts" },
+  { name: "Apps", description: "Smartphone apps and more", link: "Apps" },
+  { name: "Historical Currency Rates", description: "Check rates for any date", link: "HistoricalCurrencyRates" },
+  { name: "More Tools", link: "MoreTools" },
+];
+
+const resourcesList = [
+  { name: 'Help Center', link: '/HelpCenter' },
+  { name: 'Refer A Friend', link: '/ReferAFriend' },
+  { name: 'Blog', link: '/Blog' },
+  { name: 'Money Transfer Tips', link: '/MoneyTransferTips' },
+  { name: 'Currency Encyclopedia', link: '/CurrencyEncyclopedia' },
+  { name: 'Currency Newsletters', link: '/CurrencyNewsletters' },
+  { name: 'Glossary', link: '/Glossary' },
+  { name: 'More Resources', link: '/MoreResources' },
+];
+
+const toolsListOpen = (<div className="text-black px-5">
+  {toolsList.map((item, index) => (
+    <Link href={item.link} key={index} className="p-4 cursor-pointer hover:bg-blue-300 hover:bg-opacity-20 transition duration-300 rounded-md">
+      <h2 className="t16a24">{item.name}</h2>
+      <p className="t12a18">{item?.description}</p>
+    </Link>
+  ))}
+  </div>
+);
+
+const resourcesListOpen = (<div className=" text-black px-5">
+  {resourcesList.map((item, index) => (
+    <Link href={item.link} key={index} className="p-4 cursor-pointer hover:bg-blue-300 hover:bg-opacity-20 transition duration-300 rounded-md">
+      <div key={index}>
+        <h2>{item.name}</h2>
+      </div>
+    </Link>
+  ))}
+  </div>
+);
+
 export default function NavMenuDesctop() {
   const [activeMenuItem, setActiveMenuItem] = React.useState<string | null>(null);
   const [activeMenu, setActiveMenu] = React.useState(false);
@@ -43,8 +84,10 @@ export default function NavMenuDesctop() {
                   onClick={() => openMenu(item.name)}
                 >
                   {item.name}
-                  {item.name === "Tools" && (activeMenuItem === "Tools" ? ' ▼' : ' ►')}
+                  {item.name === "Tools" && (activeMenuItem === "Tools" ? ' ▼'  : ' ►')}
+                  {item.name === "Tools" && (activeMenuItem === "Tools" && toolsListOpen)}
                   {item.name === "Resources" && (activeMenuItem === "Resources" ? ' ▼' : ' ►')}
+                  {item.name === "Resources" && (activeMenuItem === "Resources" && resourcesListOpen)}
                 </li>
               </Link>
             )}
